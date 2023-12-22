6a32c19d7fe8f2b853c98c8a3603b5c384602be6
Hi-From-Aviral <hey.aviralgupta@gmail.com>
2023-12-22 18:21:35 +0530

    Completing Challenge

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector("button");
+btn.style.backgroundColor = 'yellow';
+
+var h1 = document.querySelector("h1");
+h1.classList.add("bigFont");
+h1.textContent = "Hi!";
+
+var anchorTag = document.querySelector("a");
+anchorTag.setAttribute("href", "https:/www.bing.com");

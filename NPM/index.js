6bfd78ed0a6ead89dac98f66ea01db7e5936aefc
Hi-From-Aviral <hey.aviralgupta@gmail.com>
2023-12-24 17:20:49 +0530

    Completing Task

---
+++ NPM/index.js
@@ -0,0 +1,4 @@
+import generateName from "sillyname"
+import superheroes from "superheroes"
+console.log(`My Name Is ${generateName()}`);
+console.log(`My Favourite Superhero Is ${superheroes.random()}`);
